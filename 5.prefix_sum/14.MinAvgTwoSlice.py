2815093e684ff1cf6a859769dea9d51d4ee4be22
tfornal <tomasz.fornal6@gmail.com>
2023-05-16 21:56:02 +0200

    .

--- 5.prefix_sum/14.MinAvgTwoSlice.py
+++ 5.prefix_sum/14.MinAvgTwoSlice.py
@@ -49,25 +49,11 @@ A = [4, 2, 2, 5, 1, 1, 5, 8]
 
 
 def min_check(avg_min, avg_tmp, idx_min, idx_tmp):
-    if avg_tmp < avg_min:
-        return idx_tmp, avg_tmp
-    else:
-        return idx_min, avg_min
+    pass
 
 
 def solution(A):
-    idx_max = len(A) + 1
-    idx_min, avg_min = 0, sum(A[0:2]) / 2
-    if idx_max == 2:
-        return idx_min
-    for idx in range(3, idx_max):
-        idx2, idx3 = idx - 2, idx - 3
-        avg_tmp2 = sum(A[idx2:idx]) / 2
-        avg_tmp3 = sum(A[idx3:idx]) / 3
-        idx_min, avg_min = min_check(avg_min, avg_tmp2, idx_min, idx2)
-        idx_min, avg_min = min_check(avg_min, avg_tmp3, idx_min, idx3)
-    print(idx_min)
-    return idx_min
+    pass
 
 
 solution(A)
